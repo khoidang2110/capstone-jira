78be2e00bc0a4d74d6913375bf69e2894d2a3ed8
khoidang2110 <khoidang2110@gmail.com>
2023-11-18 10:17:28 +0700

    login

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,7 +3,7 @@ import HeaderBar from "../components/HeaderBar/HeaderBar.jsx";
 import { Outlet, useNavigate } from "react-router-dom";
 import MenuBar from "../components/MenuBar/MenuBar.jsx";
 import PageContent from "../components/PageContent/PageContent.jsx";
-
+import LoginPage from "../pages/LoginPage/LoginPage.jsx";
 import { useDispatch, useSelector } from "react-redux";
 import { setProjectData } from "../redux/action/project.js";
 import {
@@ -14,9 +14,8 @@ import {
 import { setUsersData } from "../redux/action/userManage.js";
 
 export default function Layout() {
-  const navigate = useNavigate();
-  let { info } = useSelector((state) => state.userReducer);
-  console.log("lấy info", info);
+  // let { info } = useSelector((state) => state.userReducer);
+  // console.log("lấy info", info);
   let userJson = localStorage.getItem("USER");
   let USER = JSON.parse(userJson);
   // Outlet la nhung gi ben trong Layout
@@ -43,22 +42,27 @@ export default function Layout() {
         console.log("err", err);
       });
   }, []);
-  if (USER) {
-    return (
-      <div>
-        <HeaderBar />
-        <div className="flex">
-          <div>
-            <MenuBar />
-          </div>
-          <div>
-            <Outlet />
+
+  return (
+    <div>
+      {" "}
+      {USER ? (
+        <div>
+          <HeaderBar />
+          <div className="flex">
+            <div>
+              <MenuBar />
+            </div>
+            <div>
+              <Outlet />
+            </div>
           </div>
         </div>
-
-        {/* <PageContent/> */}
-      </div>
-    );
-  }
-  return (window.location.href = "/login");
+      ) : (
+        <div>
+          <LoginPage />
+        </div>
+      )}
+    </div>
+  );
 }
