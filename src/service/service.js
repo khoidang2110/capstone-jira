af2b2dcbd2570ae42c43faf70d1b8c71e042f165
phú đặng <phubi268@gmail.com>
2023-11-21 09:21:34 +0700

    upadte

--- src/service/service.js
+++ src/service/service.js
@@ -13,6 +13,24 @@ export let projectService = {
   getProjectList: () => {
     return https.get(`/api/Project/getAllProject`);
   },
+  getTaskPriority: () => {
+    return https.get("/api/Priority/getAll?id=0");
+  },
+  getTaskStatus: () => {
+    return https.get("/api/Status/getAll");
+  },
+  getTaskType: () => {
+    return https.get("/api/TaskType/getAll");
+  },
+  createTask: (values) => {
+    return https.post("/api/Project/createTask", values);
+  },
+  projectCategory: () => {
+    return https.get(`/api/ProjectCategory`);
+  },
+  createProjectAuthorize: (values) => {
+    return https.post(`/api/Project/createProjectAuthorize`, values);
+  },
 };
 
 export let usersManageService = {
