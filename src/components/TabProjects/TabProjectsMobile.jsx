ef393362dfbf9933820df05dfece0872e5308b64
khoidang2110 <khoidang2110@gmail.com>
2023-12-11 23:00:23 +0700

    responsive

---
+++ src/components/TabProjects/TabProjectsMobile.jsx
@@ -0,0 +1,418 @@
+import React, { useEffect, useState } from "react";
+import {
+  AutoComplete,
+  Avatar,
+  Button,
+  ConfigProvider,
+  Drawer,
+  Form,
+  Input,
+  Modal,
+  Popover,
+  Select,
+  Space,
+  Switch,
+  Table,
+  Tag,
+  message,
+} from "antd";
+import { projectService, userService } from "../../service/service";
+import { NavLink } from "react-router-dom";
+import {
+  CheckOutlined,
+  CloseOutlined,
+  DeleteOutlined,
+  EditOutlined,
+} from "@ant-design/icons";
+import { useDispatch, useSelector } from "react-redux";
+
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log("params", pagination, filters, sorter, extra);
+};
+export default function TabProjectsMobile() {
+  const [randomNumber, setRandomNumber] = useState("11");
+  const [open, setOpen] = useState(false);
+  const [form] = Form.useForm();
+  const [project, setProject] = useState("");
+  const [category, setCategory] = useState();
+
+  let userJson = localStorage.getItem("USER");
+  let USER = JSON.parse(userJson);
+  let data = JSON.parse(localStorage.getItem("USER"));
+const [projectData, setProjectData] = useState();
+  let { projectDataRedux } = useSelector((state) => state.projectReducer);
+  console.log("projectDataRedux", projectDataRedux);
+
+  // console.log(
+  //   "🚀 ~ file: TabProjects.jsx:35 ~ TabProjects ~ category:",
+  //   category
+  // );
+  // console.log(
+  //   "🚀 ~ file: TabProjects.jsx:33 ~ TabProjects ~ project:",
+  //   project
+  // );
+
+  useEffect(() => {
+    projectService
+      .projectCategory()
+      .then((res) => {
+        console.log("🚀 ~ file: TabProjects.jsx:41 ~ .then ~ res:", res);
+        setCategory(res.data.content);
+      })
+      .catch((err) => {});
+  }, []);
+  useEffect(() => {
+    projectService
+      .getProjectList()
+      .then((result) => {
+        // console.log("chay updatae");
+            setProjectData(result.data.content);
+      
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }, [randomNumber]);
+  const showDrawer = () => {
+    form.resetFields();
+    setOpen(true);
+  };
+  const onClose = () => {
+    // window.location.href = "/";
+    setOpen(false);
+    form.resetFields();
+    
+  };
+  const onFinish = (values) => {
+    console.log("🚀 ~ file: TabProjects.jsx:60 ~ onFinish ~ values:", values);
+    let dataUpdate = {
+      id: values.id,
+      projectName: values.projectName,
+      creator: data.id,
+      description: values.description,
+      // categoryId: values.category,
+      categoryId: project?.projectCategory?.id.toString(),
+    };
+    projectService
+      .updateProject(project.id, values)
+      .then((res) => {
+        message.success("Edit thành công");
+       
+        // setTimeout(() => {
+        //   window.location.href = "/";
+        // }, 1000);
+        setOpen(false);
+        setRandomNumber(Math.random());
+      })
+      .catch((err) => {
+        console.log("🚀 ~ file: TabProjects.jsx:77 ~ onFinish ~ err:", err);
+        message.error("Edit thất bại");
+      });
+    console.log(
+      "🚀 ~ file: TabProjects.jsx:70 ~ onFinish ~ dataUpdate:",
+      dataUpdate
+    );
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+ 
+  const [projectDataReduxById, setProjectDataReduxById] = useState([]);
+  const [toggleData, setToggleData] = useState([]);
+// lấy data redux
+  useEffect(() => {
+    // console.log("chạy ueff của redux");
+    if (projectDataRedux) {
+      const projectDataReduxById = projectDataRedux.filter(
+        (item) => item.creator.id == USER.id
+      );
+      setProjectData(projectDataRedux);
+      setProjectDataReduxById(projectDataReduxById);
+      setToggleData(projectDataReduxById);
+    }
+  }, [projectDataRedux]);
+  // call api data 
+  useEffect(() => {
+    // console.log("chạy ueff lay api projectdata truc tiep");
+    if (projectData) {
+      const projectDataReduxById = projectData.filter(
+        (item) => item.creator.id == USER.id
+      );
+      setProjectData(projectData);
+      setProjectDataReduxById(projectDataReduxById);
+      setToggleData(projectDataReduxById);
+    }
+  }, [projectData]);
+
+  const onChangeSwitch = (checked) => {
+    console.log(`switch to ${checked}`);
+
+    if (checked == true) {
+      setToggleData(projectDataReduxById);
+    } else if (checked == false) {
+      setToggleData(projectData);
+    }
+  };
+
+  const columns = [
+  
+    {
+      title: "Project Name",
+      dataIndex: "projectName",
+      width: 120,
+      render: (text, record, index) => {
+        return (
+          <Tag color="purple">
+            <NavLink
+              to={`/projectdetail/${record.id}`}
+              style={{ color: "#531dab", fontSize: "14px" }}
+            >
+              {text}
+            </NavLink>
+          </Tag>
+        );
+      },
+      
+    },
+
+    {
+      title: "Action",
+      key: "action",
+
+      render: (_, record) => {
+        return (
+          <>
+            <Space size="small">
+              <Button
+              type="primary"
+                className="btnBlue"
+                // type="text"
+                icon={<EditOutlined />}
+                onClick={() => {
+                  projectService
+                    .getProjectDetail(record.id)
+                    .then((res) => {
+                      setProject(res.data.content);
+                    //   setTimeout(function(){
+                    //     showDrawer();
+                    // },100);
+                     
+                      setTimeout(() => {
+                        showDrawer();
+                      }, 100);
+                    })
+                    .catch((err) => {
+                      console.log("jsx:257 ~ TabProjects ~ err:", err);
+                    });
+                }}
+              ></Button>
+              <Button
+                type="text"
+                className="btnRed"
+                icon={<DeleteOutlined />}
+                onClick={() => {
+                  setDeleteProject(record);
+                  setIsModalOpen(true);
+                }}
+              ></Button>
+            </Space>
+          </>
+        );
+      },
+    },
+  ];
+
+  // Modal Delete
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [deleteProject, setDeleteProject] = useState();
+console.log("deleteProject",deleteProject)
+  const handleOk = () => {
+    projectService
+      .deleteProject(deleteProject.id)
+      .then((res) => {
+        message.success("Xóa dự án thành công");
+        setRandomNumber(Math.random());
+        // setTimeout(() => {
+        //   window.location.href = "/";
+        // }, 1000);
+      })
+      .catch((err) => {
+        message.error("Xóa dự án thất bại");
+      })
+      .finally(setIsModalOpen(false));
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+   
+  };
+
+  return (
+    <div className="">
+      <ConfigProvider
+        theme={{
+          token: {
+            /* here is your global tokens */
+            colorPrimary: "#001529",
+            color: "red",
+          },
+          components: {
+            Form: {
+              itemMarginBottom: 20,
+              verticalLabelPadding: 1,
+            },
+          },
+        }}
+      >
+        <Switch
+          className="switch"
+          style={{ marginBottom: "25px" }}
+          checkedChildren="Your Project"
+          unCheckedChildren="All Project"
+          defaultChecked
+          onChange={onChangeSwitch}
+        />
+
+        <Drawer
+          title="Edit Project"
+          placement="right"
+          onClose={() => {
+            onClose();
+          }}
+          open={open}
+        >
+          <Form
+            form={form}
+            name="basic"
+            style={{ width: "700" }}
+            initialValues={{
+              id: project?.id,
+              projectName: project?.projectName,
+              categoryId: project.projectCategory?.name,
+              description: project?.description,
+              // remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            // layout="vertical"
+          >
+            <Form.Item
+              name="id"
+              label="ID"
+              // style={{ marginTop: "20px" }}
+              rules={[]}
+            >
+              <Input
+                style={{
+                  borderColor: "black",
+                  // borderStyle: "dashed",
+                  // height: "50px",
+                }}
+                values={project?.id}
+                disabled={true}
+              />
+            </Form.Item>
+
+            <Form.Item
+              style={{ marginTop: "20px" }}
+              name="projectName"
+              label="Project Name"
+              rules={[]}
+            >
+              <Input
+                style={{
+                  // borderColor: "black",
+                  // borderStyle: "dashed",
+                  // height: "50px",
+                }}
+                values={project?.projectName}
+              />
+            </Form.Item>
+
+            <Form.Item
+              style={{ marginTop: "20px" }}
+              name="categoryId"
+              label="Project Category"
+              rules={[]}
+            >
+              <Select
+                style={{
+                  // borderColor: "black",
+                  // borderStyle: "dashed",
+                  // height: "50px",
+                }}
+                values={{
+                  value: project?.projectCategory?.id,
+                  label: project?.projectCategory?.name,
+                }}
+              >
+                {category?.map((item, index) => {
+                  return (
+                    <Option value={item.id} key={index}>
+                      {item.projectCategoryName}
+                    </Option>
+                  );
+                })}
+              </Select>
+            </Form.Item>
+
+            <Form.Item label="Description" name="description" rules={[]}>
+              <Input.TextArea
+                rows={4}
+                style={{
+                  // borderColor: "black",
+                  // borderStyle: "dashed",
+                  height: "50px",
+                }}
+                values={project?.description}
+              />
+            </Form.Item>
+
+            <Form.Item>
+            <Space style={{width:"100%", justifyContent:"center"}}>
+              <Button
+                className="btnBlue"
+               
+                htmlType="submit"
+         
+              >
+                Submit
+              </Button>
+              <Button
+                className="btnCancel"
+                type="text"
+                onClick={() => {
+                  onClose();
+                }}
+           
+              >
+                Cancel
+              </Button>
+              </Space>
+            </Form.Item>
+          </Form>
+        </Drawer>
+
+        <Modal
+          title="Delete Project"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+         <span className="flex"> <p>Are you sure to delete this Project: </p><p className="text-red-500  pl-1">  {deleteProject?.projectName
+}</p></span>
+        </Modal>
+      </ConfigProvider>
+
+      <Table
+        columns={columns}
+        dataSource={toggleData}
+        onChange={onChange}
+        scroll={{
+          y: 280,
+        }}
+      />
+    </div>
+  );
+}
--- src/components/TabUserSetting/TabUserSetting.jsx
+++ src/components/TabUserSetting/TabUserSetting.jsx
@@ -1,239 +1,30 @@
-import { Button, ConfigProvider, Form, Input, Select, message } from "antd";
-import React from "react";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import { usersManageService } from "../../service/service";
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+
+import TabUserSettingDesktop from "./TabUserSettingDesktop";
+import TabUserSettingMobile from "./TabUserSettingMobile";
+
+const Desktop = ({ children }) => {
+  const isDesktop = useMediaQuery({ minWidth: 768 })
+  return isDesktop ? children : null
+}
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 767 })
+  return isMobile ? children : null
+}
+
 
 export default function TabUserSetting() {
-  let { usersRedux } = useSelector((state) => state.usersManageReducer);
-  console.log(
-    "🚀 ~ file: TabUserSetting.jsx:5 ~ TabUserSetting ~ usersRedux:",
-    usersRedux
-  );
-  let data = JSON.parse(localStorage.getItem("USER"));
-  console.log(
-    "🚀 ~ file: TabUserSetting.jsx:11 ~ TabUserSetting ~ data:",
-    data
-  );
-
-  const [form] = Form.useForm();
-  const [componentDisabled, setComponentDisabled] = useState(true);
-  const onFinish = (values) => {
-    let updateUser = {
-      id: values.id,
-      passWord: values.passWord,
-      email: values.email,
-      name: values.name,
-      phoneNumber: values.phoneNumber,
-    };
-    console.log(updateUser);
-    usersManageService
-      .editUser(updateUser)
-      .then((res) => {
-        message.success("Cập nhật thành công");
-      })
-      .catch((err) => {
-        message.error("Cập nhật thất bại ");
-        console.log("🚀 ~ file: TabUserSetting.jsx:36 ~ onFinish ~ err:", err);
-      });
-  };
-  const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
-  };
-
-  const prefixSelector = (
-    <Form.Item name="prefix" noStyle>
-      <Select
-        style={{
-          width: 70,
-        }}
-        defaultValue="86"
-      >
-        <Option value="86">+86</Option>
-        <Option value="87">+87</Option>
-      </Select>
-    </Form.Item>
-  );
-
   return (
-    <div className="container">
-      <div className="flex flex-col justify-center items-center">
-        <ConfigProvider
-          theme={{
-            //     token:{
-            // margin:10
-            //     },
-            components: {
-              Form: {
-                itemMarginBottom: 10,
-                verticalLabelPadding: 1,
-              },
-            },
-          }}
-        >
-          <Form
-            className=" flex flex-col align-center justify-center"
-            form={form}
-            name="register"
-            style={
-              {
-                // maxWidth: 1000,
-                // maxHeight: 1000,
-                width:300
-              }
-            }
-            initialValues={{
-              id: data.id,
-              name: data.name,
-              passWord: "",
-              email: data.email,
-              phoneNumber: data.phoneNumber,
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-            // layout="vertical"
-          >
-         
-            <div className="mb-2 font-medium text-center ">EDIT USER</div>
-            <Form.Item
-         
-              label=""
-              name="id"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your name!",
-                },
-              ]}
-            >
-              <Input
-               addonBefore="ID:"
-                style={{
-                  // borderColor: "black",
-                  // borderStyle: "dashed",
-                  // width: "400px",
-                  // height: "50px",
-                }}
-                disabled={true}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Name"
-              name="name"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your name!",
-                },
-              ]}
-            >
-              <Input
-                style={{
-                  // borderColor: "black",
-                  // borderStyle: "dashed",
-                  // width: "400px",
-                  // height: "50px",
-                }}
-                value={data.name}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Password"
-              name="passWord"
-              rules={[{ required: true }]}
-            >
-              <Input.Password
-                style={{
-                  // borderColor: "black",
-                  // borderStyle: "dashed",
-                  // width: "400px",
-                  // height: "50px",
-                }}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Email :"
-              name={"email"}
-              rules={[
-                {
-                  type: "email",
-                  required: true,
-                  message: "The email address is illegal!",
-                },
-              ]}
-            >
-              <Input
-                style={{
-                  // borderColor: "black",
-                  // borderStyle: "dashed",
-                  // width: "400px",
-                  // height: "50px",
-                }}
-                value={data.email}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Phone"
-              name="phoneNumber"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your phone number!",
-                },
-              ]}
-            >
-              <Input
-                style={{
-                  // borderColor: "black",
-                  // borderStyle: "dashed",
-                  // width: "400px",
-                  // height: "50px",
-                }}
-                // addonBefore={prefixSelector}
-                value={data.phoneNumber}
-              />
-            </Form.Item>
-
-            <Form.Item className="w-full flex justify-center items-center">
-              <>
-                {" "}
-                <Button
-                  type="text"
-                  htmlType="submit"
-                  style={{
-                    backgroundColor: "#1890ff",
-                    minWidth: "100px",
-                    borderRadius: "30px",
-                  }}
-                  className="btnBlue"
-                >
-                  Edit
-                </Button>
-                <Button
-                  className="px-3 mx-2 btnCancel"
-                  type="text"
-                  onClick={() => {
-                    window.location.href = "/";
-                  }}
-                  style={{
-                    backgroundColor: "#808080",
-                    color: "white",
-                    borderRadius: "30px",
-                    minWidth: "100px",
-                  }}
-                >
-                  Cancel
-                </Button>
-              </>
-            </Form.Item>
-          </Form>
-        </ConfigProvider>
-      </div>
+    <div>
+        <Desktop>
+            <TabUserSettingDesktop/>
+        </Desktop>
+        <Mobile>
+            <TabUserSettingMobile/>
+        </Mobile>
+  
     </div>
-  );
+  )
 }
