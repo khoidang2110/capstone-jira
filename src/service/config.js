acef853b11ae4986512da8492bcf5ddb4a004d3f
khoidang2110 <khoidang2110@gmail.com>
2024-01-22 11:35:39 +0700

    token update

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import {getAccessToken} from "../utils"
 const TOKEN_CYBERSOFT =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NiIsIkhldEhhblN0cmluZyI6IjAzLzA0LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxMjEwMjQwMDAwMCIsIm5iZiI6MTY4MzMwNjAwMCwiZXhwIjoxNzEyMjUwMDAwfQ.YeDhc_oSixV2XtFPDzcpxFhBos5832JpQpndHNoqZLk";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NiIsIkhldEhhblN0cmluZyI6IjI1LzA0LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxNDAwMzIwMDAwMCIsIm5iZiI6MTY4MzMwNjAwMCwiZXhwIjoxNzE0MTUwODAwfQ.4kX1rB3Flu3UAMyzUxN5Iu1_zKMNahLuhAHkwdSzOx0";
 export let https = axios.create({
   baseURL: "https://jiranew.cybersoft.edu.vn",
   headers: {
@@ -11,7 +11,7 @@ export let https = axios.create({
   
 });
 
-
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NiIsIkhldEhhblN0cmluZyI6IjAzLzA0LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxMjEwMjQwMDAwMCIsIm5iZiI6MTY4MzMwNjAwMCwiZXhwIjoxNzEyMjUwMDAwfQ.YeDhc_oSixV2XtFPDzcpxFhBos5832JpQpndHNoqZLk";
 
 
 
